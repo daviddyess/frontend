363377c381fd0a9d98cf90585dfff8ddbe07ef17
daviddyess <david.dyess@gmail.com>
2019-11-22 09:14:51 -0700

    Prepped FlavorStash page for redux

--- src/pages/user/FlavorStash.js
+++ src/pages/user/FlavorStash.js
@@ -1,14 +1,43 @@
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+// import { bindActionCreators } from 'redux';
+import { Col, Container, Row } from 'react-bootstrap';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default class FlavorStash extends Component {
+export class FlavorStash extends Component {
+  static propTypes = {
+    actions: PropTypes.object.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <Container>
         <Helmet title="Your Flavor Stash" />
-        <h1>User Flavor Stash</h1>
+        <Row className="text-center">
+          <Col>
+            <h1>Flavor Stash</h1>
+          </Col>
+        </Row>
       </Container>
     );
   }
 }
+/*
+const mapStateToProps = state => ({
+  // collection: getAllFlavors(state),
+  // pager: getFlavorsPager(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  // actions: bindActionCreators(flavorsActions, dispatch)
+});
+*/
+export default connect()(FlavorStash);
+// mapStateToProps,
+// mapDispatchToProps
