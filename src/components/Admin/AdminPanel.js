3506a6eeb5f868ce4acb25a6f1325b42c108f7ab
daviddyess <david.dyess@gmail.com>
2020-09-01 17:20:27 -0600

    Added Breadcrumbs and Panels to Admin page

--- src/components/Admin/AdminPanel.js
+++ src/components/Admin/AdminPanel.js
@@ -1,12 +1,32 @@
 import { Helmet } from 'react-helmet';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Breadcrumbs from 'components/Breadcrumbs/Breadcrumbs';
 import { Col } from 'react-bootstrap';
 
 export default function AdminPanel() {
+  const panels = [
+    {
+      name: 'Roles',
+      url: '/admin/roles'
+    }
+  ];
+
   return (
     <Col>
       <Helmet title="Administration - Home" />
+      <Breadcrumbs base="home" active="Admin" />
       The new admin panel
+      <ul>
+        {panels &&
+          panels.map((panel, index) => {
+            return (
+              <li key={index}>
+                <Link to={panel.url}>{panel.name}</Link>
+              </li>
+            );
+          })}
+      </ul>
     </Col>
   );
 }
